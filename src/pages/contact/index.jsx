c00cab81f1c868be204547e59ba03cb7aa993926
allesonsales <allesonsales@gmail.com>
2025-02-04 15:15:08 -0300

    updated files

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -13,26 +13,35 @@ const icon = () => {
                 </div>
                 <div className="contatoIcons">
                     <div className="icon">
-                        <a href="#">11 96618-6601</a>
-                        <i class="bi bi-whatsapp"></i>
+                        <a href="https://wa.me/5511966186601" target='blank'>
+                            11 96618-6601
+                            <i class="bi bi-whatsapp"></i>
+                        </a>
                     </div>
                     <div className="icon">
-                        <a href="#">allesonsales@gmail.com</a>
-                        <i class="bi bi-envelope-at-fill"></i>
+                        <a href="mailto:allesonsales@gmail.com">
+                            allesonsales@gmail.com
+                            <i class="bi bi-envelope-at-fill"></i>
+                        </a>
                     </div>
                     <div className="icon">
-                        <a href=''>LinkedIn </a>
-                        <i class="bi bi-linkedin"></i>
+                        <a href='https://www.linkedin.com/in/allesonsales/' target='blank'>
+                            LinkedIn 
+                            <i class="bi bi-linkedin"></i>
+                        </a>
                     </div>
                     <div className="icon">
-                        <a href=''>Instagram</a>
-                        <i class="bi bi-instagram"></i>
+                        <a href=''>
+                            Instagram
+                            <i class="bi bi-instagram"></i>
+                        </a>
                     </div>
                     <div className="icon">
-                        <a href='#'>GitHub</a>
-                        <i className="bi bi-github"></i>
+                        <a href='https://github.com/allesonsales' target='blank'>
+                            GitHub
+                            <i className="bi bi-github"></i>
+                        </a>
                     </div>
-
                 </div>
             </div>
         </section>
