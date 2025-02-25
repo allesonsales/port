b45cd96a3e55ff006339c037b1fdb9cf9096e8d2
allesonsales <allesonsales@gmail.com>
2025-02-25 18:12:42 -0300

    layout mobile atualizado

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -19,15 +19,16 @@ useEffect(() => {
                 <h2>Projetos</h2>
             </div>
             <div className="projectsContainer">    
-                {list?.map(({ name,link, image, url, description }, index) => (
+                {list?.map(({ name,link, image, url, description, git }, index) => (
                     <div className="item">
-                    <a href={link} target='blank'>
-                        <div className='background' style={{ backgroundImage: `url(${image})`}} key = {index}>
-                        </div>
-                        <span className='projectTitle'>{name}</span>
-                        <span className='projectDescription'>{description}</span>
-                    </a>
-                </div>
+                        <a href={link} target='blank'>
+                            <div className='background' style={{ backgroundImage: `url(${image})`}} key = {index}>
+                            </div>
+                            <span className='projectTitle'>{name}</span>
+                            <span className='projectDescription'>{description}</span>
+                        </a>
+                        <a className='git' href={git}>Código do projeto</a>
+                    </div>
             ))}
             </div>
         </section>
